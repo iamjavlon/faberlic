d580cd7faa8285e729d39b357f9640aa985917f7
Javlon <javlon20048@gmail.com>
2021-10-25 17:24:00 +0500

    created cart and products apps and put secret key into .env

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4esp46b(kz_(yzo!h*%)l&p2oeoa!x@07h(3@q%4ml7!pdo%0#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
