44e8cca4986c2f4e87e7ef8bb6bbdccf8d6a2a3d
Javlon <javlon20048@gmail.com>
2021-11-01 15:16:27 +0500

    django fix

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,6 @@ dotenv.load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -42,8 +41,6 @@ INSTALLED_APPS = [
 
     #own
     'users',
-    'products',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     class Meta:
         verbose_name_plural = "Пользователи"
-        verbose_name = "пользователя"
+        verbose_name = "   пользователя"
 
     LANGUAGES = [
         ('en', "English"),
@@ -14,9 +14,9 @@ class User(models.Model):
 
     id = models.BigIntegerField(primary_key=True, verbose_name="ID")
     first_name = models.CharField(
-        max_length=255, null = True, verbose_name='Имя', blank=True)
+        max_length=255, null=True, verbose_name="Имя", blank=True)
     last_name = models.CharField(
-        max_length=255, null = True, verbose_name='Фамилия', blank=True)
+        max_length=255, null=True, verbose_name="Фамилия", blank=True)
     username = models.CharField(
         max_length=255, null=True, blank=True, verbose_name="Юзернейм")
     phone_number = models.PositiveBigIntegerField(
